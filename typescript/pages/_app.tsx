d5273a85eda8aa713cb3a4af799af6c8f1ead9e6
mikan3rd <hiroki.ota@zenkigen.jp>
2022-07-16 22:45:38 +0900

    chore: add App.tsx

--- typescript/pages/_app.tsx
+++ typescript/pages/_app.tsx
@@ -1,8 +1,40 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import "antd/dist/antd.css";
+
+import { devConfig } from '../config/dev';
+import { generateVideoToken } from '../utils/util';
+import ZoomVideo from '@zoom/videosdk';
+import ZoomContext from '../context/zoom-context';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+
+let meetingArgs: any = Object.fromEntries(new URLSearchParams(location.search));
+if (
+  !meetingArgs.sdkKey ||
+  !meetingArgs.topic ||
+  !meetingArgs.name ||
+  !meetingArgs.signature
+) {
+  meetingArgs = devConfig;
+}
+if (!meetingArgs.signature && meetingArgs.sdkSecret && meetingArgs.topic) {
+  meetingArgs.signature = generateVideoToken(
+    meetingArgs.sdkKey,
+    meetingArgs.sdkSecret,
+    meetingArgs.topic,
+    meetingArgs.password,
+    'jack4',
+    'jack'
+  );
+}
+console.log('meetingArgs', meetingArgs);
+const zmClient = ZoomVideo.createClient();
+
+  return (
+    <ZoomContext.Provider value={zmClient}>
+  <Component {...pageProps} meetingArgs={meetingArgs as any} />
+  </ZoomContext.Provider>)
 }
 
 export default MyApp
--- typescript/pages/index.tsx
+++ typescript/pages/index.tsx
@@ -3,69 +3,162 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
-const Home: NextPage = () => {
+import React, {
+  useEffect,
+  useContext,
+  useState,
+  useCallback,
+  useReducer,
+} from "react";
+import ZoomVideo, { ConnectionState, event_connection_change, event_media_sdk_change } from "@zoom/videosdk";
+import { message, Modal } from "antd";
+import ZoomContext from "../context/zoom-context";
+import { ChatClient, MediaStream } from "../types/index-types";
+import produce from "immer";
+
+interface AppProps {
+  meetingArgs: {
+    sdkKey: string;
+    topic: string;
+    signature: string;
+    name: string;
+    password?: string;
+  };
+}
+const mediaShape = {
+  audio: {
+    encode: false,
+    decode: false,
+  },
+  video: {
+    encode: false,
+    decode: false,
+  },
+  share: {
+    encode: false,
+    decode: false,
+  },
+};
+const mediaReducer = produce((draft, action) => {
+  switch (action.type) {
+    case "audio-encode": {
+      draft.audio.encode = action.payload;
+      break;
+    }
+    case "audio-decode": {
+      draft.audio.decode = action.payload;
+      break;
+    }
+    case "video-encode": {
+      draft.video.encode = action.payload;
+      break;
+    }
+    case "video-decode": {
+      draft.video.decode = action.payload;
+      break;
+    }
+    case "share-encode": {
+      draft.share.encode = action.payload;
+      break;
+    }
+    case "share-decode": {
+      draft.share.decode = action.payload;
+      break;
+    }
+    default:
+      break;
+  }
+}, mediaShape);
+
+const Home: NextPage<AppProps> = (props) => {
+
+  const {
+    meetingArgs: { sdkKey, topic, signature, name, password },
+  } = props;
+  const [loading, setIsLoading] = useState(true);
+  const [loadingText, setLoadingText] = useState("");
+  const [isFailover, setIsFailover] = useState<boolean>(false);
+  const [status, setStatus] = useState<string>("closed");
+  const [mediaState, dispatch] = useReducer(mediaReducer, mediaShape);
+  const [mediaStream, setMediaStream] = useState<MediaStream | null>(null);
+  const [chatClient, setChatClient] = useState<ChatClient | null>(null);
+  const [isSupportGalleryView, setIsSupportGalleryView] = useState<boolean>(true);
+  const zmClient = useContext(ZoomContext);
+
+  useEffect(() => {
+    const init = async () => {
+      await zmClient.init("en-US", `${window.location.origin}/lib`, 'zoom.us');
+      try {
+        setLoadingText("Joining the session...");
+        await zmClient.join(topic, signature, name, password);
+        const stream = zmClient.getMediaStream();
+        setMediaStream(stream);
+	      setIsSupportGalleryView(stream.isSupportMultipleVideos());
+        const chatClient = zmClient.getChatClient();
+        setChatClient(chatClient);
+        setIsLoading(false);
+      } catch (e: any) {
+        setIsLoading(false);
+        message.error(e.reason);
+      }
+    };
+    init();
+    return () => {
+      ZoomVideo.destroyClient();
+    };
+  }, [sdkKey, signature, zmClient, topic, name, password]);
+  const onConnectionChange = useCallback(
+    (payload: Parameters<typeof event_connection_change>[0]) => {
+      if (payload.state === ConnectionState.Reconnecting) {
+        setIsLoading(true);
+        setIsFailover(true);
+        setStatus("connecting");
+        const { reason } = payload;
+        if (reason === "failover") {
+          setLoadingText("Session Disconnected,Try to reconnect");
+        }
+      } else if (payload.state === ConnectionState.Connected) {
+        setStatus("connected");
+        if (isFailover) {
+          setIsLoading(false);
+        }
+      } else if (payload.state === ConnectionState.Closed) {
+        setStatus("closed");
+        if (payload.reason === "ended by host") {
+          Modal.warning({
+            title: "Meeting ended",
+            content: "This meeting has been ended by host",
+          });
+        }
+      }
+    },
+    [isFailover]
+  );
+  const onMediaSDKChange = useCallback((payload: Parameters<typeof event_media_sdk_change>[0]) => {
+    const { action, type, result } = payload;
+    dispatch({ type: `${type}-${action}`, payload: result === "success" });
+  }, []);
+  const onLeaveOrJoinSession = useCallback(async () => {
+    if (status === "closed") {
+      setIsLoading(true);
+      await zmClient.join(topic, signature, name, password);
+      setIsLoading(false);
+    } else if (status === "connected") {
+      await zmClient.leave();
+      message.warn("You have left the session.");
+    }
+  }, [zmClient, status, topic, signature, name, password]);
+  useEffect(() => {
+    zmClient.on("connection-change", onConnectionChange);
+    zmClient.on("media-sdk-change", onMediaSDKChange);
+    return () => {
+      zmClient.off("connection-change", onConnectionChange);
+      zmClient.off("media-sdk-change", onMediaSDKChange);
+    };
+  }, [zmClient, onConnectionChange, onMediaSDKChange]);
+
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <meta name="description" content="Generated by create next app" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.tsx</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
-    </div>
+    <div>TEST</div>
   )
 }
 
